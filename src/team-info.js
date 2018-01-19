3f63702aa532b756615f58df9113d26480dd98a1
Alline Coelho <alline.coelho@corp.globo.com>
2018-01-19 02:18:27 -0200

    Adds desktop fix

--- src/team-info.js
+++ src/team-info.js
@@ -12,9 +12,8 @@ const TeamInfo = () => (
 					<strong className="team-email">emaildotime@corp.globo.com</strong>
 					<div className="team-description">
 						Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o 
-						século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de 
-						tipos.
-						</div>
+						século XVI.
+					</div>
 				</div>
 			</div>
 )
